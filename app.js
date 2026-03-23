fed23b2a99bb27825b5ba5cfc9b833f8f458c056
Nikita Yurishev <chelovek438@gmail.com>
2026-03-23 21:50:49 +0300

    new solution hope it works

--- app.js
+++ app.js
@@ -1,64 +1,30 @@
-const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas, true);
-const instructions = document.getElementById("instructions");
+const startButton = document.getElementById('startButton');
 
-const createScene = async function () {
-    const scene = new BABYLON.Scene(engine);
+const start = async() => {
+    startButton.style.display = "none";
+
+    const mindarThree = new window.MINDAR.IMAGE.MindARBabylon({
+        container: document.querySelector("#container"),
+        imageTargetSrc: './marker.mind', 
+    });
+
+    const {engine, scene, camera} = await mindarThree.start();
+
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 1.0;
-    const isSupported = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync("immersive-ar");
-    
-    if (!isSupported) {
-        instructions.innerText = "Ваш браузер не поддерживает AR. Используйте Chrome на Android или специальный браузер на iOS.";
-        return scene;
-    }
 
-
-    const xr = await scene.createDefaultXRExperienceAsync({
-        uiOptions: {
-            sessionMode: "immersive-ar",
-            referenceSpaceType: "local-floor"
+    const anchor = mindarThree.addAnchor(0); 
+    BABYLON.SceneLoader.ImportMesh("", "./", "model.glb", scene, (meshes) => {
+        const root = meshes[0];
+        root.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);    
+        anchor.group.addChild(root);
+        if (scene.animationGroups.length > 0) {
+            scene.animationGroups[0].play(true);
         }
     });
 
-    instructions.innerText = "Нажмите на кнопку AR в углу и наведите на QR-код";
-
-    const featuresManager = xr.baseExperience.featuresManager;
-    
-
-    const imageTracking = featuresManager.enableFeature(BABYLON.WebXRFeaturesManager.JS_WEBXR_IMAGE_TRACKING, "latest", {
-        images: [
-            {
-                src: "marker.png", 
-                estimatedRealWorldWidth: 0.1 
-            }
-        ]
-    });
-
-    // Загрузка модели
-    BABYLON.SceneLoader.ImportMesh("", "./", "model.glb", scene, function (meshes) {
-        const wolf = meshes[0];
-        wolf.setEnabled(false); 
-        imageTracking.onTrackedImageUpdatedObservable.add((image) => {
-            instructions.style.display = "none"; 
-            wolf.setEnabled(true);
-            
-            // Позиционирование
-            image.transformationMatrix.decompose(wolf.scaling, wolf.rotationQuaternion, wolf.position);
-            
-            if (scene.animationGroups.length > 0) {
-                scene.animationGroups[0].play(true);
-            }
-        });
-    });
-
-    return scene;
-};
-
-createScene().then((scene) => {
     engine.runRenderLoop(() => {
         scene.render();
     });
-});
+}
 
-window.addEventListener("resize", () => engine.resize());
+startButton.addEventListener('click', start);
